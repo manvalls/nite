2941b3c3c0389e5c60f96a2f18d6adc8997b5660
Manuel Valls Fernández <manolo@vlrz.es>
2016-09-18 20:04:37 +0200

    Improve support for small screens

--- src/view.js
+++ src/view.js
@@ -39,66 +39,74 @@ module.exports = (ctrl, { forEach, when }) =>
   </div>
 
   {
-    forEach(ctrl.samples,sample =>
-      <div>
+    forEach(ctrl.samples,(sample, i) =>
 
-        {{
-          style: {
-            display: 'flex',
-            flexWrap: 'wrap',
-            alignItems: 'flex-start',
-            justifyContent: 'center',
-            margin: '25px 0px'
-          }
-        }}
-
-        <div style={{flexGrow: 1,margin: '25px', maxWidth: '750px'}}>
-          <div>
-
-            { forEach(Object.keys(sample.code.files),file =>
-              <div onclick={e => sample.code.selected.set(file)} style={{display: 'inline-block', padding: '10px',userSelect: 'none'}}>
-
-                {
-                  when(sample.code.selected.is(file),{
-                    style: {
-                      cursor: 'default',
-                      backgroundColor: '#282c34',
-                      color: '#dedede'
-                    }
-                  }).else({
-                    style: {
-                      cursor: 'pointer',
-                      backgroundColor: 'white',
-                      color: 'black'
-                    }
-                  })
-                }
-
-                {file}
-              </div>
-            ) }
-
-          </div>
-          <div>{{innerHTML: sample.code.selected.to(file => sample.code.files[file])}}</div>
-        </div>
-
+      <frag>
         <div>
 
           {{
             style: {
-              margin: '25px',
-              marginTop: 'calc(1.15em + 45px)',
-              padding: '20px',
-              border: '1px solid #cacaca',
-              backgroundColor: 'rgb(244, 244, 244)'
+              display: 'flex',
+              flexWrap: 'wrap',
+              alignItems: 'flex-start',
+              justifyContent: 'center',
+              margin: '25px 0px'
             }
           }}
 
-          {sample.result}
+          <div style={{flexGrow: 1,margin: '25px', maxWidth: '750px'}}>
+            <div>
+
+              { forEach(Object.keys(sample.code.files),file =>
+                <div onclick={e => sample.code.selected.set(file)} style={{display: 'inline-block', padding: '10px',userSelect: 'none'}}>
+
+                  {
+                    when(sample.code.selected.is(file),{
+                      style: {
+                        cursor: 'default',
+                        backgroundColor: '#282c34',
+                        color: '#dedede'
+                      }
+                    }).else({
+                      style: {
+                        cursor: 'pointer',
+                        backgroundColor: 'white',
+                        color: 'black'
+                      }
+                    })
+                  }
+
+                  {file}
+                </div>
+              ) }
+
+            </div>
+            <div>{{innerHTML: sample.code.selected.to(file => sample.code.files[file])}}</div>
+          </div>
+
+          <div>
+
+            {{
+              style: {
+                margin: '25px',
+                marginTop: 'calc(1.15em + 45px)',
+                padding: '20px',
+                border: '1px solid #cacaca',
+                backgroundColor: 'rgb(244, 244, 244)',
+                wordBreak: 'break-word'
+              }
+            }}
+
+            {sample.result}
+
+          </div>
 
         </div>
 
-      </div>
+        { when(i.lt(ctrl.samples.length - 1),<hr style={{width: '90%'}}/>) }
+
+      </frag>
+
     )
   }
 
