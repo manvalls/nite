b055c9c682c722b6e3dede39619d5a72e9191942
Manuel Valls Fernández <manolo@vlrz.es>
2017-02-11 22:16:30 +0100

    Bump dependencies

--- std/promises.js
+++ std/promises.js
@@ -1,8 +1,9 @@
 var Resolver = require('y-resolver');
 
 exports.defer = Resolver.defer;
-exports.resolve = Resolver.accept;
+exports.resolve = Resolver.resolve;
 exports.reject = Resolver.reject;
 exports.race = Resolver.race;
 exports.all = Resolver.all;
+exports.when = Resolver.when;
 exports.get = Resolver.Yielded.get;
